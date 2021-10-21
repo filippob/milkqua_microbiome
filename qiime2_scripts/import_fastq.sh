dd4afd9325548fbfeb1fc05966f812274116989e
Filippo Biscarini <filippo.biscarini.est@login2.indaco.unimi.it>
2021-10-21 16:56:31 +0200

    modified output path

--- qiime2_scripts/import_fastq.sh
+++ qiime2_scripts/import_fastq.sh
@@ -2,5 +2,5 @@
 qiime tools import  --type 'SampleData[PairedEndSequencesWithQuality]' \
   --input-path $HOME/MILKQUA/data/subset \
   --input-format CasavaOneEightSingleLanePerSampleDirFmt \
-  --output-path demux-paired-end.qza
+  --output-path $HOME/MILKQUA/Analysis/demux-paired-end.qza
 
