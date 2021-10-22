b20ee8dd103b3e401971c2aab5a4598438facd7c
Filippo Biscarini <filippo.biscarini.est@login3.indaco.unimi.it>
2021-10-22 11:58:34 +0200

    adding another function to import fastq data

--- qiime2_scripts/import_fastq.sh
+++ qiime2_scripts/import_fastq.sh
@@ -1,6 +1,11 @@
 
-qiime tools import  --type 'SampleData[PairedEndSequencesWithQuality]' \
-  --input-path $HOME/MILKQUA/data/subset \
-  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
-  --output-path $HOME/MILKQUA/Analysis/demux-paired-end.qza
+#qiime tools import  --type 'SampleData[PairedEndSequencesWithQuality]' \
+#  --input-path $HOME/MILKQUA/data/subset \
+#  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
+#  --output-path $HOME/MILKQUA/Analysis/demux-paired-end.qza
 
+qiime tools import \
+  --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path $HOME/MILKQUA/Config/subset_manifest.tsv \
+  --output-path $HOME/MILKQUA/Analysis/paired-end-demux.qza \
+  --input-format PairedEndFastqManifestPhred33V2
