0984492cb66b498ab3bd8dcf2a8e5da26df1b0cd
filippob <filippo.biscarini.est@login3.indaco.unimi.it>
2021-10-22 12:02:57 +0200

    commenting the script

--- qiime2_scripts/import_fastq.sh
+++ qiime2_scripts/import_fastq.sh
@@ -1,9 +1,12 @@
+## Importing data with different formats
 
+## CASAVA foramt
 #qiime tools import  --type 'SampleData[PairedEndSequencesWithQuality]' \
 #  --input-path $HOME/MILKQUA/data/subset \
 #  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
 #  --output-path $HOME/MILKQUA/Analysis/demux-paired-end.qza
 
+## Paired-end reads with quality (fastq files)
 qiime tools import \
   --type 'SampleData[PairedEndSequencesWithQuality]' \
   --input-path $HOME/MILKQUA/Config/subset_manifest.tsv \
