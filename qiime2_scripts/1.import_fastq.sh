dc2c96e7060f98a1e9e8db7cdc62643e89518372
Filippo Biscarini <filippo.biscarini@gmail.com>
2021-12-10 12:44:56 +0100

    adding qiime scripts to process milkqua data: 1. ann 2.1.

--- qiime2_scripts/1.import_fastq.sh
+++ qiime2_scripts/1.import_fastq.sh
@@ -1,4 +1,20 @@
+#!/bin/sh
+
+## script that imports into Qiime2 already demultiplexed data
+## input data format is fastq: R1 and R2 files (forward and reverse)
+## sequence still contain the barcode (to be trimmed later)
+## (the function `qiime demux emp-paired` demultiplexes multiplexed data and leave the barcode intact)
+## type:
+## input-format:
+
 ## Importing data with different formats
+
+project_home="$HOME/MILKQUA"
+data_folder="210902_M04028_0139_000000000-JRGYP"
+
+echo "project folder is $project_home"
+echo " - running qiime to import data"
+
 
 ## CASAVA foramt
 #qiime tools import  --type 'SampleData[PairedEndSequencesWithQuality]' \
@@ -9,6 +25,6 @@
 ## Paired-end reads with quality (fastq files)
 qiime tools import \
   --type 'SampleData[PairedEndSequencesWithQuality]' \
-  --input-path $HOME/MILKQUA/Config/subset_manifest.tsv \
-  --output-path $HOME/MILKQUA/Analysis/paired-end-demux.qza \
+  --input-path $project_home/Config/manifest.csv \
+  --output-path $project_home/Analysis/$data_folder.qza \
   --input-format PairedEndFastqManifestPhred33V2
---
+++ qiime2_scripts/2.1.subsample.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+## script that subsamples demultiplexed data
+## input data format is fastq: R1 and R2 files (forward and reverse) already imported into Qiime2
+## sequences still contain the barcode (to be trimmed later)
+## (the function `qiime demux emp-paired` demultiplexes multiplexed data and leave the barcode intact)
+
+project_home="$HOME/MILKQUA"
+data_folder="210902_M04028_0139_000000000-JRGYP"
+
+echo "project folder is $project_home"
+echo " - running qiime for subsampling"
+qiime demux subsample-paired --i-sequences $project_home/Analysis/$data_folder.qza --p-fraction 0.1 --o-subsampled-sequences $project_home/Analysis/${data_folder}_subsample.qza
+
+echo " - running qiime for summary"
+qiime demux summarize --i-data $project_home/Analysis/${data_folder}_subsample.qza --o-visualization $project_home/Analysis/${data_folder}_subsample.qzvi
+
+echo "DONE!"
+
