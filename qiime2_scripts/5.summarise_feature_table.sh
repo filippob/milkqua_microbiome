53fa5b601a2b8f77bda2c263abd3fafff5fa27b7
Filippo Biscarini <filippo.biscarini@gmail.com>
2021-12-10 17:48:32 +0100

    adding description to the script

--- qiime2_scripts/5.summarise_feature_table.sh
+++ qiime2_scripts/5.summarise_feature_table.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## script that trims and truncates reads (a little quality filtering)
+## script that summarises and visualises the results from DADA2 (feature table, representative sequences, quality filtering statistics)
 
 project_home="$HOME/MILKQUA"
 data_folder="210902_M04028_0139_000000000-JRGYP"
@@ -13,12 +13,9 @@ module load python3/intel/2020
 source activate qiime2-2019.10
 
 echo " - running qiime to summarise the feature table"
-
 qiime feature-table summarize --i-table $project_home/Analysis/${data_folder}_table.qza --o-visualization $project_home/Analysis/${data_folder}_table.qzv --m-sample-metadata-file $project_home/Config/mapping_file.tsv
-
-qiime feature-table tabulate-seqs --i-data rep-seqs.qza --o-visualization rep-seqs.qzv
-
-qiime metadata tabulate --m-input-file denoising-stats.qza --o-visualization denoising-stats.qzv
+qiime feature-table tabulate-seqs --i-data $project_home/Analysis/${data_folder}_rep-seqs.qza --o-visualization $project_home/Analysis/${data_folder}_rep-seqs.qzv
+qiime metadata tabulate --m-input-file $project_home/Analysis/${data_folder}_denoising-stats.qza --o-visualization $project_home/Analysis/${data_folder}_denoising-stats.qzv
 
 echo "DONE!"
 
