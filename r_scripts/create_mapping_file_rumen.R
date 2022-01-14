86690c8b8455dc435255ac7bcc460664cc225ea8
filippob <filippo.biscarini@gmail.com>
2022-01-14 09:45:09 +0100

    modified r script to prepare mapping file for qiime etc.

--- r_scripts/create_mapping_file_rumen.R
+++ r_scripts/create_mapping_file_rumen.R
@@ -1,13 +1,14 @@
 library("tidyverse")
+library("data.table")
 
-mapping <- readxl::read_excel("milkqua_rumen_samples.xlsx")
+mapping <- readxl::read_excel("mapping_file_rumen.xlsx")
 
 ## renaming
-names(mapping) <- c("nid","exp","vial","treatment","dosis","inoculation","replicate")
+mapping = rename(mapping, `#SampleID` = `#sampleID`)
 
 ## create duplicated id and reorder
-mapping$`#sampleID` <- paste(mapping$nid,mapping$nid,sep="")
-mapping <- relocate(mapping, `#sampleID`, .before = nid)
+# mapping$`#sampleID` <- paste(mapping$nid,mapping$nid,sep="")
+# mapping <- relocate(mapping, `#sampleID`, .before = nid)
 
 ## write out mapping file
 fwrite(x = mapping, file = "mapping_file.csv", sep = "\t", row.names = FALSE, col.names = TRUE)
