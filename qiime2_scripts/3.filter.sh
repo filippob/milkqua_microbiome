f3ed2c1e974bc0beb9e8b30a4182aa347bd8d6d4
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-03-25 13:24:00 +0100

    adding parameters

--- qiime2_scripts/3.filter.sh
+++ qiime2_scripts/3.filter.sh
@@ -6,6 +6,12 @@
 project_home="$HOME/MILKQUA"
 data_folder="210902_M04028_0139_000000000-JRGYP"
 min_seqs=100
+manifest="Config/manifest_milk.csv"
+outdir="Analysis/milkqua_milk_subset"
+
+if [ ! -d "${outdir}/filter" ]; then
+	mkdir -p ${outdir}/filter
+fi
 
 echo "project folder is $project_home"
 
@@ -15,15 +21,15 @@ module load python3/intel/2020
 source activate qiime2-2019.10
 
 echo " - running qiime for exporting data (to get information on number of reads per sample)"
-qiime tools export --input-path $project_home/Analysis/${data_folder}_subsample.qzv --output-path $project_home/Analysis/demux-subsample_temp/
-sed -i 's/,/\t/g' $project_home/Analysis/demux-subsample_temp/per-sample-fastq-counts.csv
-tail -n +2 $project_home/Analysis/demux-subsample_temp/per-sample-fastq-counts.csv > $project_home/Analysis/temp
-echo -e "sample-id\tforward sequence count" | cat - $project_home/Analysis/temp > $project_home/Analysis/demux-subsample_temp/per-sample-fastq-counts.csv
-rm $project_home/Analysis/temp
+qiime tools export --input-path $project_home/$outdir/subsample/${data_folder}_subsample.qzv --output-path $project_home/$outdir/filter/demux-subsample_temp/
+sed -i 's/,/\t/g' $project_home/$outdir/filter/demux-subsample_temp/per-sample-fastq-counts.csv
+tail -n +2 $project_home/$outdir/filter/demux-subsample_temp/per-sample-fastq-counts.csv > $project_home/Analysis/temp
+echo -e "sample-id\tforward sequence count" | cat - $project_home/$outdir/filter/temp > $project_home/$outdir/filter/demux-subsample_temp/per-sample-fastq-counts.csv
+rm $project_home/$outdir/filter/temp
 
 echo " - running qiime for filtering samples"
 echo " - removing samples with fewer than $min_seqs reads"
-qiime demux filter-samples --i-demux $project_home/Analysis/${data_folder}_subsample.qza --m-metadata-file $project_home/Analysis/demux-subsample_temp/per-sample-fastq-counts.csv --p-where "CAST([forward sequence count] AS INT) > $min_seqs" --o-filtered-demux $project_home/Analysis/${data_folder}_filtered.qza
+qiime demux filter-samples --i-demux $project_home/$outdir/filter/${data_folder}_subsample.qza --m-metadata-file $project_home/Analysis/demux-subsample_temp/per-sample-fastq-counts.csv --p-where "CAST([forward sequence count] AS INT) > $min_seqs" --o-filtered-demux $project_home/Analysis/${data_folder}_filtered.qza
 
 echo " - removing temporary folders"
 rm -r $project_home/Analysis/demux-subsample_temp
