0a7bf9c9790eb6616d749b0f0aa00acf8209a7d7
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-03-25 11:40:49 +0100

    adding parameters

--- qiime2_scripts/2.1.subsample.sh
+++ qiime2_scripts/2.1.subsample.sh
@@ -6,7 +6,13 @@
 ## (the function `qiime demux emp-paired` demultiplexes multiplexed data and leave the barcode intact)
 
 project_home="$HOME/MILKQUA"
-data_folder="210902_M04028_0139_000000000-JRGYP"
+data_folder="210902_M04028_0139_000000000-JRGYP_milk"
+manifest="Config/manifest_milk.csv"
+outdir="Analysis/milkqua_milk_subset"
+
+if [ ! -d "${outdir}/subsample" ]; then
+	mkdir -p ${outdir}/subsample
+fi
 
 echo "project folder is $project_home"
 
@@ -16,10 +22,10 @@ module load python3/intel/2020
 source activate qiime2-2019.10
 
 echo " - running qiime for subsampling"
-qiime demux subsample-paired --i-sequences $project_home/Analysis/$data_folder.qza --p-fraction 0.3 --verbose --o-subsampled-sequences $project_home/Analysis/${data_folder}_subsample.qza
+qiime demux subsample-paired --i-sequences $project_home/$outdir/import/$data_folder.qza --p-fraction 0.3 --verbose --o-subsampled-sequences $project_home/$outdir/subsample/${data_folder}_subsample.qza
 
 echo " - running qiime for summary"
-qiime demux summarize --i-data $project_home/Analysis/${data_folder}_subsample.qza --o-visualization $project_home/Analysis/${data_folder}_subsample.qzv
+qiime demux summarize --i-data $project_home/$outdir/subsample/${data_folder}_subsample.qza --o-visualization $project_home/$outdir/subsample/${data_folder}_subsample.qzv
 
 echo "DONE!"
 
