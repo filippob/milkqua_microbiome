bf214d7652b322c4cd91ea64b5a9eee3537f4621
filippob <filippo.biscarini@gmail.com>
2022-03-25 10:41:34 +0100

    fixing bugs

--- r_scripts/create_manifest.r
+++ r_scripts/create_manifest.r
@@ -4,11 +4,11 @@ library("data.table")
 #########################
 ## PARAMETERS   #########
 #########################
-mapping_file = "mapping_file.csv.xlsx"
+mapping_file = "~/Documents/cremonesi/milkqua/mapping_file.csv.xlsx"
 type = "milk"
 abs_path = "/home/users/filippo.biscarini.est/MILKQUA/data/210902_M04028_0139_000000000-JRGYP_milk"
 n_samples = 10 ## controls the number of samples randomly selected
-outdir = ""
+outdir = "Config/"
 
 ## reading the data
 writeLines(" - reading the mapping file")
@@ -17,7 +17,7 @@ mapping$`sample-id` = paste("sample",mapping$Sample_ID, sep="-")
 mapping = rename(mapping, sample_2n = `#SampleID`) %>% relocate(`sample-id`, .before = sample_2n)
 
 writeLines(" - filtering samples")
-print(paste("selecting", sample_type, "samples"))
+print(paste("selecting", type, "samples"))
 mapping <- filter(mapping, sample_type == type)
 
 ## create manifest file
@@ -54,8 +54,8 @@ manifest <- manifest %>%
          `reverse-absolute-filepath` = reverse_absolute_filepath)
 
 writeLines(" - writing out the manifest file")
-fname = paste(outdir, "manifest.csv", sep="/")
+fname = paste(outdir, "manifest_",type,".csv", sep="")
 print(paste("writing to file", fname))
-fwrite(file = fname, sep="\t", col.names = TRUE)
+fwrite(manifest, file = fname, sep="\t", col.names = TRUE)
 
 print("DONE!!")
