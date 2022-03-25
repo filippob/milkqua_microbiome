af932824910e6243e61b45d71ac5e64774050e3f
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-03-25 16:44:22 +0100

    add parameters (check mapping file)

--- qiime2_scripts/5.summarise_feature_table.sh
+++ qiime2_scripts/5.summarise_feature_table.sh
@@ -4,6 +4,12 @@
 
 project_home="$HOME/MILKQUA"
 data_folder="210902_M04028_0139_000000000-JRGYP"
+manifest="Config/manifest_milk.csv"
+outdir="Analysis/milkqua_milk_subset"
+
+if [ ! -d "${outdir}/filter" ]; then
+	mkdir -p ${outdir}/filter
+fi
 
 echo "project folder is $project_home"
 
@@ -13,9 +19,10 @@ module load python3/intel/2020
 source activate qiime2-2019.10
 
 echo " - running qiime to summarise the feature table"
-qiime feature-table summarize --i-table $project_home/Analysis/${data_folder}_table.qza --o-visualization $project_home/Analysis/${data_folder}_table.qzv --m-sample-metadata-file $project_home/Config/mapping_file.tsv
-qiime feature-table tabulate-seqs --i-data $project_home/Analysis/${data_folder}_rep-seqs.qza --o-visualization $project_home/Analysis/${data_folder}_rep-seqs.qzv
-qiime metadata tabulate --m-input-file $project_home/Analysis/${data_folder}_denoising-stats.qza --o-visualization $project_home/Analysis/${data_folder}_denoising-stats.qzv
+echo "(input from denoise/)"
+qiime feature-table summarize --i-table $project_home/$outdir/denoise/${data_folder}_table.qza --o-visualization $project_home/$outdir/denoise/${data_folder}_table.qzv --m-sample-metadata-file $project_home/Config/mapping_file.tsv
+qiime feature-table tabulate-seqs --i-data $project_home/$outdir/denoise/${data_folder}_rep-seqs.qza --o-visualization $project_home/$outdir/denoise/${data_folder}_rep-seqs.qzv
+qiime metadata tabulate --m-input-file $project_home/$outdir/denoise/${data_folder}_denoising-stats.qza --o-visualization $project_home/$outdir/denoise/${data_folder}_denoising-stats.qzv
 
 echo "DONE!"
 
