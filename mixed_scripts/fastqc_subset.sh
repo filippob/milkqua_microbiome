c1065939e841b84470f97122ed0044f976447b17
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-04-01 13:24:27 +0200

    adding checks, variables and comments

--- mixed_scripts/fastqc_subset.sh
+++ mixed_scripts/fastqc_subset.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+## script to run fastqc and multiqc on a subset of the samples
+## the subset is defined by a numeric range via seq start end
+
 ## parameters
 project_home=$HOME/MILKQUA
 input_dir="220225_M04028_0144_000000000-K6CMG"
 targetdir="Analysis/milkqua_stools/0.fastqc"
 multiqc_sif="singularity_containers/multiqc-1.9.sif"
+nstart=45  #number of first sample to include in the subset
+nend=76    #number of last sample to include in the subset
 
 cd $project_home
 
 ## create temporary folder
 if [ ! -d ${project_home}/data/temp ]; then
-
+	
+	echo " - creating temporary folder"
 	mkdir ${project_home}/data/temp
 
 	## copying files of interest in the temporary folder
@@ -33,26 +39,27 @@ source activate /gpfs/home/projects/MILKQUA/Conda/env-arriba/
 
 ## running fastqc on single samples
 if [ ! -d $targetdir ]; then
-	echo "creating trarget dir"
+	echo " - creating trarget dir"
        	mkdir $targetdir
 fi
 
 if [ -z "$(ls -A $targetdir)" ]; then
-	echo "folder is empty"
-	echo "running fastqc on single samples"
+	echo "(target folder is empty)"
+	echo " - running fastqc on single samples"
         fastqc data/temp/*.fastq.gz -o $targetdir -t 8
         #singularity run ${project_home}/${multiqc_sif} fastqc data/temp/*.fastq.gz -o $targetdir -t 8
 fi
 
 ## MultiQC
 cd $targetdir
-echo "running multiqc on fastqc files"
+echo " - running multiqc on fastqc files"
 #multiqc .
-singularity run ${project_home}/${multiqc_sif} .
+singularity run ${project_home}/${multiqc_sif} -f .
 
 ## removing temporary folder
 cd $project_home
-#rm -r data/temp
+echo " - removing temporary folder"
+rm -r data/temp
 
 echo "done"
 
