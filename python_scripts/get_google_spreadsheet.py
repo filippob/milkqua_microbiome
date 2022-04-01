e2a3c09495eb3ced4547f55e08a9dad2f5a0d3f6
filippob <filippo.biscarini@gmail.com>
2022-04-01 16:26:27 +0200

    first stub of script to download google spreadsheets

---
+++ python_scripts/get_google_spreadsheet.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 15:43:11 2022
+
+@author: filippo
+"""
+
+#%% import libraries
+import os
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+#%% set params
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive']
+docid = "12qfJKcg3hHhG3210pim878cTvMSGFsmR0YET_qFs55E"
+home = '/home/filippo/'
+credential_path = 'Dropbox/gdrive/microbiomes-6719db38cddb.json'
+project = 'Documents/MILKQUA'
+label = 'milkqua_stools_swabs.csv'
+
+#%%
+fname = os.path.join(home, credential_path)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(fname, scope)
+client = gspread.authorize(credentials)
+google_sh = client.open_by_key(docid)
+
+#%%
+sheet1 = google_sh.get_worksheet(0)
+
+#%%
+df = pd.DataFrame(data=sheet1.get_all_records())
+
+#%% write out
+fname = os.path.join(home, project,label)
+df.to_csv(fname, index=False)
