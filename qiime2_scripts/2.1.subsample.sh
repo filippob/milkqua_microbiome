24b0ca1e6df8a8c7cf235c29d4628b0dbbf37350
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-04-08 15:00:05 +0200

    changing permissions to output folders + selectiong conda/singularity

--- qiime2_scripts/2.1.subsample.sh
+++ qiime2_scripts/2.1.subsample.sh
@@ -6,9 +6,11 @@
 ## (the function `qiime demux emp-paired` demultiplexes multiplexed data and leave the barcode intact)
 
 project_home="$HOME/MILKQUA"
-data_folder="210902_M04028_0139_000000000-JRGYP_milk"
-manifest="Config/manifest_milk.csv"
-outdir="Analysis/milkqua_milk_subset"
+data_folder="220225_M04028_0144_000000000-K6CMG"
+manifest="Config/manifest_faeces.csv"
+outdir="Analysis/milkqua_stools"
+fraction=0.33
+use_singularity=true #if false the conda env is used
 
 if [ ! -d "${outdir}/subsample" ]; then
 	mkdir -p ${outdir}/subsample
@@ -16,16 +18,36 @@ fi
 
 echo "project folder is $project_home"
 
-## exporting conda to PATH then activating the Qiime2 Conda env
-echo " - activating conda env"
-module load python3/intel/2020
-source activate qiime2-2019.10
+if [ "${use_singularity}" = true ]; then
+	
+	echo "SINGULARITY (chosen qiime distribution)"
+	echo " - running qiime for subsampling"	
+	singularity run /gpfs/software/Container/qiime2_2022.2.sif \
+	qiime demux subsample-paired --i-sequences $project_home/$outdir/import/${data_folder}.qza \
+	 --p-fraction $fraction --verbose --o-subsampled-sequences $project_home/$outdir/subsample/${data_folder}_subsample.qza
 
-echo " - running qiime for subsampling"
-qiime demux subsample-paired --i-sequences $project_home/$outdir/import/${data_folder}.qza --p-fraction 0.3 --verbose --o-subsampled-sequences $project_home/$outdir/subsample/${data_folder}_subsample.qza
+	echo " - running qiime for summary"
+	singularity run /gpfs/software/Container/qiime2_2022.2.sif \
+	qiime demux summarize --i-data $project_home/$outdir/subsample/${data_folder}_subsample.qza --o-visualization $project_home/$outdir/subsample/${data_folder}_subsample.qzv
 
-echo " - running qiime for summary"
-qiime demux summarize --i-data $project_home/$outdir/subsample/${data_folder}_subsample.qza --o-visualization $project_home/$outdir/subsample/${data_folder}_subsample.qzv
+else
+	echo "CONDA ENV (chosen qiime distribution)"
+	## exporting conda to PATH then activating the Qiime2 Conda env
+	echo " - activating conda env"
+	module load python3/intel/2020
+	source activate qiime2-2019.10
+
+	echo " - running qiime for subsampling"
+	qiime demux subsample-paired --i-sequences $project_home/$outdir/import/${data_folder}.qza \
+	--p-fraction $fraction --verbose --o-subsampled-sequences $project_home/$outdir/subsample/${data_folder}_subsample.qza
+
+	echo " - running qiime for summary"
+	qiime demux summarize --i-data $project_home/$outdir/subsample/${data_folder}_subsample.qza --o-visualization $project_home/$outdir/subsample/${data_folder}_subsample.qzv
+
+fi
+
+echo " - changing permissions to the output folder (r+x)"
+chmod -R a+rx ${project_home}/${outdir}/subsample
 
 echo "DONE!"
 
