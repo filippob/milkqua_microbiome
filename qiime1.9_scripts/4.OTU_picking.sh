a383b32edcc3e38ffd71c4860534a577fc98179d
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-04-22 16:47:09 +0200

    adding code to convert biom to tsv

--- qiime1.9_scripts/4.OTU_picking.sh
+++ qiime1.9_scripts/4.OTU_picking.sh
@@ -4,31 +4,35 @@
 
 ## setting up the environment
 currpath=$(pwd)
-project_home="/gpfs/home/users/chiara.gini/MILKQUA"
+project_home="$HOME/MILKQUA"
 data_folder="Analysis/milkqua_skinswab/qiime1.9/3.quality_filtering"
 output_dir="Analysis/milkqua_skinswab/qiime1.9/4.OTU_picking"
-param_file="temp/OnTest_Chiara/Databases/Silva_132/SILVA_132_QIIME_release"
+dbpath="Databases/Silva_132/SILVA_132_QIIME_release"
+#sing_container="${project_home}/Qiime1.9.sif"
+sing_container="/gpfs/software/Container/qiime_docker:fischuu-qiime-1.9.1.sif"
 
 cd $currpath
 echo "project folder is $project_home"
 
 ## make folder if it does not exist
 
-if [ ! -d "$project_home/${output_dir}/4.OTU_picking" ]; then
-        mkdir -p $project_home/${output_dir}/4.OTU_picking
+if [ ! -d "$project_home/${output_dir}" ]; then
+        mkdir -p $project_home/${output_dir}
         chmod g+rxw $project_home/${output_dir}
 fi
 
 ## using the Singularity container
 
-echo " - calling the singularity container"
-
-singularity run /gpfs/software/Container/qiime_docker:fischuu-qiime-1.9.1.sif \
+echo " - calling the singularity container for OTU picking"
+singularity run ${sing_container} \
         pick_closed_reference_otus.py \
-        --reference_fp $project_home/${param_file}/rep_set/rep_set_all/97/silva132_97.fna \
-        --taxonomy_fp $project_home/${param_file}/taxonomy/taxonomy_all/97/raw_taxonomy.txt \
+        --reference_fp $project_home/$dbpath/rep_set/rep_set_all/97/silva132_97.fna \
+        --taxonomy_fp $project_home/$dbpath/taxonomy/taxonomy_all/97/raw_taxonomy.txt \
         --force --parallel --jobs_to_start=32 \
         --input_fp=$project_home/${data_folder}/seqs.fna \
-        --output_dir=$project_home/${output_dir}/4.OTU_picking
+        --output_dir=$project_home/${output_dir}
+
+echo " - converting biom file to tsv file"
+singularity run ${sing_container} biom convert -i otu_table.biom -o otu_table.txt --to-tsv --header-key taxonomy
 
 echo "DONE!"
