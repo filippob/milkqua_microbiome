bc87ffc8ce3f1898820e6aa65d516fff270fccc1
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-04-22 11:15:51 +0200

    removing code to cpoy files in the temp folder (now a separate scripts prepares the data)

--- qiime1.9_scripts/1.extract_barcode.sh
+++ qiime1.9_scripts/1.extract_barcode.sh
@@ -7,35 +7,19 @@
 ## setting up the environment
 currpath=$(pwd)
 project_home="$HOME/MILKQUA"
-data_folder="data/220225_M04028_0144_000000000-K6CMG"
+#data_folder="data/220225_M04028_0144_000000000-K6CMG"
 output_dir="Analysis/milkqua_stools/qiime1.9/extract_barcode"
 #sing_container="${project_home}/Qiime1.9.sif"
 sing_container="/gpfs/software/Container/qiime_docker:fischuu-qiime-1.9.1.sif"
 temp_folder="temp/temp_fastq"
-sample_start=45 #first sample to use (in the sequence)
-sample_end=55 #last sample to use (in the sequence)
 
 cd $currpath
 echo "project folder is $project_home"
 
-## copying files of interest to process
-## e.g. K6CMG samples 45 - 76 is stools
-
-echo " - creating temporary folder for sequence data ${temp_folder}"
-if [ ! -d "${temp_folder}" ]; then
-        mkdir -p ${temp_folder}
-fi
-
-echo " - copying relevant fastq files"
-for i in $(seq ${sample_start} ${sample_end}); 
-do
-	cp ${data_folder}/${i}*.fastq.gz ${project_home}/${temp_folder}
-done
-
-
-## make folder if it does not exist
+## make output folder if it does not exist
 if [ ! -d "${output_dir}" ]; then
 	mkdir -p ${output_dir}
+	chmod g+rxw ${output_dir}
 fi
 
 ## using the Singularity container
@@ -43,7 +27,7 @@ echo " - calling the singularity container"
 singularity run ${sing_container} multiple_extract_barcodes.py --input_dir=${temp_folder} --output_dir=${output_dir} --read1_indicator _R1 --read2_indicator _R2
 
 echo " - removing temprorary files"
-rm -r ${temp_folder}
+#rm -r ${temp_folder}
 
 echo "DONE!"
 
