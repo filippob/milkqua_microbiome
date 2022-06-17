3c68432eb7c6d22a5a1bcd4d01bd5f5f397aa038
Filippo Biscarini <filippo.biscarini@gmail.com>
2022-06-17 14:53:47 +0200

    commenting the script

--- mixed_scripts/fastqc.sh
+++ mixed_scripts/fastqc.sh
@@ -1,22 +1,28 @@
 ## setting the enviornmnent
 currpath=$(pwd)
-datapath="data/subset"
-targetdir="Analysis/raw_quality/subset"
+datapath="data/mock-sequences"
+targetdir="Analysis/mock_communities/0.fastqc"
 core=8
 
+## load the conda env with multiqc installed
 module load python3/intel/2020
-source activate milkqua
+#source activate milkqua
+source activate /gpfs/home/projects/MILKQUA/Conda/env-arriba
 
+## MULTIQC can also be invoked from here (for some users who have multiqc installed in their locale)
+## Exporting the path to bin by the way, also makes available additional software which may be useful
+#export PATH=/home/users/luiz.dematos/.local/bin:$PATH
 export PATH=/home/users/filippo.biscarini.est/.local/bin:$PATH
-## Create analysis folders
 
+## Create analysis folders
 echo "current path: $currpath"
 
 targetpath="$currpath/$targetdir"
 echo "target path: $targetpath"
 
 if [ ! -d $targetpath ]; then
-	mkdir $targetpath
+	mkdir -p $targetpath
+	chmod g+rxw $targetpath
 fi
 
 
